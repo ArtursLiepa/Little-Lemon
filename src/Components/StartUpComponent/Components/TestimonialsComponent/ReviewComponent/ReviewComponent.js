c5029c8fe5149ac632ced295194f3c0d8f500f69
Arturs Liepa <arturs.liepa85@gmail.com>
2026-06-06 01:11:33 +0300

    update Testimonials component

--- src/Components/StartUpComponent/Components/TestimonialsComponent/ReviewComponent/ReviewComponent.js
+++ src/Components/StartUpComponent/Components/TestimonialsComponent/ReviewComponent/ReviewComponent.js
@@ -1,9 +1,14 @@
 import "./ReviewComponent.css";
+import { useTheme } from "../../../../../Context/ThemeContext";
 
 const Review = (props) => {
+  const { theme } = useTheme();
   const List = props.data.map((items) => {
     return (
-      <div className="reviewCard" key={items.id}>
+      <div
+        className={`reviewCard ${theme === "light" ? "lightcards" : "darkcards"}`}
+        key={items.id}
+      >
         <section className="reviewRating"> Rate Score: {items.rating}</section>
         <section className="reviewAuthor">
           <div className="authorPhoto">
